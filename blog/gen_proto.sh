1950f9159968bb86f54b2b38c1cbe16570a3b4fd
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2019-02-16 23:07:52 +0100

    Add useful Makefile + script responsible for proto files compilation

---
+++ blog/gen_proto.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:. \
+  blogpb/blog.proto
+
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true,request_context=true:. \
+  blogpb/blog.proto
